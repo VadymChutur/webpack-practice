9c997a165d27548b6f6d186d2584527d8991d081
Vadym Chutur <falloutgreck@gmail.com>
2022-09-30 21:23:30 +0300

    update

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  entry: '/src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'my-first-webpack.bundle.js',
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 8585,
+  },
+};
